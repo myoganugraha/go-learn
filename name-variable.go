1bb676c9107cdbeab69b75335c9c8fd347e14df7
M. Yoga Nugraha <myoganugraha@gmail.com>
2020-07-04 01:03:52 +0700

    #

--- name-variable.go
+++ name-variable.go
@@ -5,8 +5,9 @@ import "fmt"
 func main() {
 	var firstName string = "john"
 	var lastName = "Doe"
-
+	
+	age := 20
 	middleName := "Aw" //short declaration
 
-	fmt.Printf("haloo %s %s %s \n", firstName, middleName, lastName)
+	fmt.Printf("haloo %s %s %s, age %d \n", firstName, middleName, lastName, age)
 }
